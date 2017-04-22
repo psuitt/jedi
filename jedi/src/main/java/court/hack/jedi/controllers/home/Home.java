7577616bd5adde14eb186ace0b6516d0d398e780
JUDICIAL\b88ch <hadfield.christopher@gmail.com>
2017-04-22 17:32:26 -0600

    Menubar switching webpages

--- jedi/src/main/java/court/hack/jedi/controllers/home/Home.java
+++ jedi/src/main/java/court/hack/jedi/controllers/home/Home.java
@@ -1,12 +1,16 @@
 package court.hack.jedi.controllers.home;
 
 import court.hack.jedi.controllers.HtmlPageController;
+import court.hack.jedi.pojo.MenuItem;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by b888pcs on 4/22/2017.
@@ -18,6 +22,34 @@ public class Home extends HtmlPageController {
     @Produces(MediaType.APPLICATION_JSON)
     public Response get() {
         return Response.ok(getHtmlPage("pages/home.html"), MediaType.TEXT_HTML_TYPE).build();
+    }
+
+    @GET
+    @Path("/menu")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getMenu() {
+        List<MenuItem> menuItemList = new LinkedList<>();
+        MenuItem menuItem = new MenuItem();
+        menuItem.setName("List of Users");
+        menuItem.setUrl("/jedi/resources/pages/users.html");
+        menuItemList.add(menuItem);
+        menuItem = new MenuItem();
+        menuItem.setName("Tasks");
+        menuItem.setUrl("/jedi/resources/pages/task.html");
+        menuItemList.add(menuItem);
+        menuItem = new MenuItem();
+        menuItem.setName("Calendar");
+        menuItem.setUrl("/jedi/resources/pages/calendar.html");
+        menuItemList.add(menuItem);
+        menuItem = new MenuItem();
+        menuItem.setName("Create Task");
+        menuItem.setUrl("/jedi/resources/pages/create_task.html");
+        menuItemList.add(menuItem);
+        menuItem = new MenuItem();
+        menuItem.setName("Create Account");
+        menuItem.setUrl("/jedi/resources/pages/create_account.html");
+        menuItemList.add(menuItem);
+        return Response.ok(menuItemList, MediaType.APPLICATION_JSON).build();
     }
 
 }
---
+++ jedi/src/main/webapp/resources/static/js/home.js
@@ -0,0 +1,84 @@
+$(document).ready(function () {
+    ajax({
+        url: "/jedi/api/home/menu",
+        type: "GET",
+        success: function (data) {
+            var menu = $("#menu");
+            for(var i = 0; i < data.length; i++){
+                var menuItem = $('<p>',{
+                    text: data[i].name,
+                    class: 'mdl-navigation__link mdl-typography--text-uppercase'
+                });
+                menuItem.attr("url", data[i].url);
+                menuItem.click(function(){
+                    renderView($(this).attr("url"));
+                });
+                menuItem.appendTo(menu);
+            }
+        }
+    });
+});
+
+function renderView(url){
+    $("#viewer").load(url);
+}
+
+function ajax(ajaxOptions) {
+    "use strict";
+    var defaultOptions = {
+        type: "GET",
+        dataType: "json",
+        context: this,
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        cache: false,
+        blockElement: undefined,
+        onStatus401: function(){ window.location.reload(); },
+    };
+    var options = $.extend(defaultOptions, ajaxOptions);
+
+    if(options["blockElement"]){
+        $(options["blockElement"]).block({
+            message: options["blockContent"],
+            overrideJPODMessage: true
+        });
+    }
+
+    $.ajax({
+        type: options["type"],
+        url: options["url"],
+        data: options["data"],
+        dataType: options["dataType"],
+        context: options["context"],
+        cache: options["cache"],
+        headers: options["headers"],
+        success: function (data) {
+            if(options["success"]){
+                options["success"].call(options["context"], data);
+            }
+        },
+        error: function (xhr, status, error) {
+            if (xhr.responseJSON) {
+                xhr.responseJSON.ErrorMessage = xhr.responseJSON.ErrorMessage || "Server responded with out custom ErrorMessage";
+            } else if (xhr.status === 401){
+                options.onStatus401.call(options.context);
+            } else if (xhr.status === 405){
+                console.error(xhr);
+            }
+            if(options["error"]) {
+                options["error"].call(options["context"], xhr, status, error);
+            }
+        },
+        complete: function () {
+            if(options["complete"]){
+                options["complete"].call(options["context"]);
+            }
+            if(options["blockElement"]){
+                $(options["blockElement"]).unblock();
+            }
+        }
+    });
+}
+
