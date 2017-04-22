7802f2788ca2a64e8a2dfbb33b97db66f30ac161
suitt, phillip <phillip.suitt@judicial.state.co.us>
2017-04-22 09:33:27 -0600

    First commit

---
+++ src/main/java/court/hack/jedi/Application.java
@@ -0,0 +1,7 @@
+package court.hack.jedi;
+
+/**
+ * Created by b888pcs on 4/22/2017.
+ */
+public class Application {
+}
---
+++ src/main/java/court/hack/jedi/controllers/heartbeat/Heartbeat.java
@@ -0,0 +1,7 @@
+package court.hack.jedi.controllers.heartbeat;
+
+/**
+ * Created by b888pcs on 4/22/2017.
+ */
+public class Heartbeat {
+}
