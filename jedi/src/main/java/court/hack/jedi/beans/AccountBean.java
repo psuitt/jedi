baff6d5e0791088d2fe95940028bc84216a15d11
John Zigler <john.zigler@judicial.state.co.us>
2017-04-22 17:47:09 -0600

    Initial commit of AccountRepository.

---
+++ jedi/src/main/java/court/hack/jedi/beans/AccountBean.java
@@ -0,0 +1,55 @@
+package court.hack.jedi.beans;
+
+public class AccountBean {
+	private String accountId;
+	private String email;
+	private String password;
+	private String accountType;
+	private String firstName;
+	private String lastName;
+	private String phoneNumber;
+	
+	
+	public String getAccountId() {
+		return accountId;
+	}
+	public void setAccountId(String accountId) {
+		this.accountId = accountId;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getAccountType() {
+		return accountType;
+	}
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+}
---
+++ jedi/src/main/java/court/hack/jedi/repositories/AccountRepository.java
@@ -0,0 +1,91 @@
+package court.hack.jedi.repositories;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.util.StringUtils;
+
+import court.hack.jedi.beans.AccountBean;
+
+public class AccountRepository {
+	private static final String GET_ACCOUNT_BY_EMAIL = "SELECT * FROM ACCOUNT WHERE EMAIL = ?";
+	private static final String INSERT_ACCOUNT = "INSERT INTO ACCOUNT"
+			+ " (EMAIL, PASSWORD, ACCOUNT_TYPE, FIRST_NAME, LAST_NAME, PHONE_NUMBER)"
+			+ " VALUES (?, ?, ?, ?, ?, ?)";
+	
+	private static final RowMapper<AccountBean> ACCOUNT_ROW_MAPPER = new RowMapper<AccountBean>() {
+		@Override
+		public AccountBean mapRow(ResultSet rs, int arg1) throws SQLException {
+			return new AccountBean();
+		}
+	};
+	
+	public AccountBean getAccountByEmail(final String email) {
+		if (email == null || email.isEmpty()) {
+			return null;
+		}
+
+		Context ctx;
+		try {
+			ctx = new InitialContext();
+			DataSource ds = (DataSource)ctx.lookup("jdbc/jedi");
+			Connection connection = ds.getConnection();
+			PreparedStatement ps = connection.prepareStatement(GET_ACCOUNT_BY_EMAIL);
+			ps.setString(0, email);
+			ResultSet rs = ps.executeQuery();
+			return ACCOUNT_ROW_MAPPER.mapRow(rs, 0);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	/**
+	 * Insert an account
+	 * @return an error string, or null if successful
+	 */
+	public String insertAccount(final AccountBean account) {
+		if (account == null) {
+			return "Account Bean is null.";
+		} else if (StringUtils.isEmpty(account.getEmail())) {
+			return "Account email is necessary.";
+		} else if (StringUtils.isEmpty(account.getPassword())) {
+			return "Account password is necessary.";
+		} else if (StringUtils.isEmpty(account.getAccountType())) {
+			return "Account type is necessary.";
+		} else if (StringUtils.isEmpty(account.getFirstName())) {
+			return "First name is necessary.";
+		} else if (StringUtils.isEmpty(account.getLastName())) {
+			return "Last name is necessary.";
+		} else {
+			Context ctx;
+			try {
+				ctx = new InitialContext();
+				DataSource ds = (DataSource)ctx.lookup("jdbc/jedi");
+				Connection connection = ds.getConnection();
+				PreparedStatement ps = connection.prepareStatement(INSERT_ACCOUNT);
+				ps.setString(0, account.getEmail());
+				ps.setString(0, account.getPassword());
+				ps.setString(0, account.getAccountType());
+				ps.setString(0, account.getFirstName());
+				ps.setString(0, account.getLastName());
+				ps.setString(0, account.getPhoneNumber());
+				if (ps.executeUpdate() == 1) {
+					return null;
+				} else {
+					return "No rows were inserted";
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				return e.getMessage();
+			}
+		}
+	}
+}
