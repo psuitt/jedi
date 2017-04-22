3003a8008c9d741f1fffdcae7936553f67852bcf
psuitt <suitt.phillip@live.com>
2017-04-22 17:43:18 -0600

    Tasks

--- jedi/src/main/java/court/hack/jedi/controllers/task/Task.java
+++ jedi/src/main/java/court/hack/jedi/controllers/task/Task.java
@@ -7,6 +7,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.Date;
 
 /**
  * Created by b888pcs on 4/22/2017.
@@ -20,5 +21,48 @@ public class Task extends HtmlPageController {
         return Response.ok(getHtmlPage("pages/task.html"), MediaType.TEXT_HTML_TYPE).build();
     }
 
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("json")
+    public Response getJson() {
+        return Response.ok(new TaskItem("Test", "text desc", new Date()), MediaType.APPLICATION_JSON).build();
+    }
+
+    public class TaskItem {
+
+        private String title;
+        private String desc;
+        private Date date;
+
+        public TaskItem(final String title, final String desc, final Date date) {
+            this.title = title;
+            this.desc = desc;
+            this.date = date;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(final String title) {
+            this.title = title;
+        }
+
+        public String getDesc() {
+            return desc;
+        }
+
+        public void setDesc(final String desc) {
+            this.desc = desc;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(final Date date) {
+            this.date = date;
+        }
+    }
 
 }
--- jedi/src/main/webapp/resources/static/js/task.js
+++ jedi/src/main/webapp/resources/static/js/task.js
@@ -1,23 +1,117 @@
 /**
  * Created by b888pcs on 4/22/2017.
  */
-// Passing a named function instead of an anonymous function.
-
-court = court || {};
+var court = court || {};
 court.hack = court.hack || {};
 court.hack.task = court.hack.task || {};
 
 court.hack.task = function() {
 
+    var _list = [
+        {title: "Title 1", desc: "Description 1", date: "12/1/2017", time: "10:10pm"},
+        {title: "Title 2", desc: "Description 2", date: "12/2/2017", time: "10:10pm"},
+        {title: "Title 3", desc: "Description 3", date: "12/3/2017", time: "10:10pm"},
+        {title: "Title 4", desc: "Description 4", date: "12/4/2017", time: "10:10pm"},
+        {title: "Title 5", desc: "Description 5", date: "12/5/2017", time: "10:10pm"},
+        {title: "Title 6", desc: "Description 6", date: "12/6/2017", time: "10:10pm"}
+    ];
+
     var _loadTasks = function() {
 
-        
+        var tasks = $("#tasks");
 
+        tasks.html(" ");
+
+        _.each(_list, _loadTask);
+
+    };
+
+    var _loadTask = function(value, index) {
+
+        var tasks = $("#tasks");
+        var li = $("<li></li>");
+
+        li.addClass("mdl-list__item mdl-list__item--two-line");
+
+        var spanPrimary = $("<span></span>");
+        var spanTitle = $("<span></span>");
+        var spanDescription = $("<span></span>");
+
+        spanPrimary.addClass("mdl-list__item-primary-content");
+
+        spanTitle.addClass("span");
+
+        var col1 = $("<span></span>");
+        col1.addClass("col-1");
+        col1.html(value.title);
+
+        spanTitle.append(col1);
+
+        var col2 = $("<span></span>");
+        col2.addClass("col-2");
+        col2.html(value.date);
+
+        spanTitle.append(col2);
+
+        var col3 = $("<span></span>");
+        col3.addClass("col-3");
+        col3.html(value.time);
+
+        spanTitle.append(col3);
+
+        spanDescription.addClass("mdl-list__item-sub-title");
+        spanDescription.text(value.desc);
+
+        spanPrimary.append(spanTitle);
+        spanPrimary.append(spanDescription);
+
+        li.append(spanPrimary);
+
+        var spanSecondary = $("<span></span>");
+
+        spanSecondary.addClass("mdl-list__item-secondary-action");
+
+        var label = $("<label></label>");
+
+        label.attr("for", "input-" + index);
+        label.addClass("mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect");
+
+        var input = $("<input/>");
+
+        input.attr("id", "input-" + index);
+        input.attr("type", "checkbox");
+        input.addClass("mdl-checkbox__input");
+
+        label.append(input);
+
+        spanSecondary.append(label);
+
+        li.append(spanSecondary);
+
+        tasks.append(li);
+
+        tasks.upgradeElement();
+
+    };
+
+    var _init = function() {
+        _loadTasks();
+
+        $("#taskAdd").click(function() {
+            var inputs = $("#taskInputs input");
+            var task =  {
+                title: inputs.eq(0).val(),
+                desc: inputs.eq(1).val(),
+                date: inputs.eq(2).val(),
+                time: inputs.eq(3).val()
+            };
+            _loadTask(task, $("#tasks li").length);
+        });
     };
 
     return {
         init: function() {
-            _loadTasks();
+            _init();
         }
     }
 
